1da5fd1702c969101b8bcf089ca50caf097f99c4
AmanyFekry <amanyfekry200045@gmail.com>
2023-12-19 03:32:26 +0200

    edit

--- src/app.js
+++ src/app.js
@@ -15,95 +15,95 @@ MongoClient.connect(url, (error, res1) =>
     const db = res1.db(dbname)
 //task 1 insertOne 2
 
-    // db.collection('users').insertOne({
-    //     _id: 1,
-    //     name: 'amany',
-    //     age: 27
-    // }, (error, data) =>
-    // {
-    //     if (error)
-    //     {
-    //         console.log("Unable to insert data")
-    //     }
-    //     console.log(data.insertedId)
-    // })
+    db.collection('users').insertOne({
+        _id: 1,
+        name: 'amany',
+        age: 27
+    }, (error, data) =>
+    {
+        if (error)
+        {
+            console.log("Unable to insert data")
+        }
+        console.log(data.insertedId)
+    })
 
-    // db.collection('users').insertOne({
-    //     _id: 2,
-    //     name: 'Karim',
-    //     age: 27
-    // }, (error, data) =>
-    // {
-    //     if (error)
-    //     {
-    //         console.log("Unable to insert data")
-    //     }
-    //     console.log(data.insertedId)
-    // })
+    db.collection('users').insertOne({
+        _id: 2,
+        name: 'Karim',
+        age: 27
+    }, (error, data) =>
+    {
+        if (error)
+        {
+            console.log("Unable to insert data")
+        }
+        console.log(data.insertedId)
+    })
     // ///////////////////////////
-    // //insertMany 10 5 of them age =27
-    // db.collection('users').insertMany(
-    //     [
-    //         {
-    //             _id:3,
-    //             name: 'islam',
-    //             age: 27
-    //         },
-    //         {
-    //             _id:4,
-    //             name: 'Ahmed',
-    //             age: 27
-    //         },
-    //         {
-    //             _id: 5,
-    //             name: 'ayla',
-    //             age: 30
+    //insertMany 10 5 of them age =27
+    db.collection('users').insertMany(
+        [
+            {
+                _id:3,
+                name: 'islam',
+                age: 27
+            },
+            {
+                _id:4,
+                name: 'Ahmed',
+                age: 27
+            },
+            {
+                _id: 5,
+                name: 'ayla',
+                age: 30
 
-    //         },
-    //         {
-    //             _id:6,
-    //             name: 'ali',
-    //             age: 35
-    //         },
-    //         {
-    //             _id: 7,
-    //             name: 'Mohamed',
-    //             age: 27
-    //         },
-    //         {  
-    //             _id:8,
-    //             name: "amira",
-    //             age:24
-    //         },
-    //         {
-    //             _id :9 ,
-    //             name: "Mariam",
-    //             age: 32
-    //         },
-    //         {
-    //             _id:10,
-    //             name: 'rami',
-    //             age: 24
-    //         },
-    //         {
-    //             _id: 11,
-    //             name: 'sara',
-    //             age:30
-    //         },
-    //         {
-    //             _id: 12,
-    //             name: "adel",
-    //             age: 40
-    //         }
-    //     ], (error, data) =>
-    // {
-    //     if (error)
-    //     {
-    //         console.log("Error in inserting multiple documents");
-    //     }
-    //     console.log(data.insertedCount)
-    // }
-    // )
+            },
+            {
+                _id:6,
+                name: 'ali',
+                age: 35
+            },
+            {
+                _id: 7,
+                name: 'Mohamed',
+                age: 27
+            },
+            {  
+                _id:8,
+                name: "amira",
+                age:24
+            },
+            {
+                _id :9 ,
+                name: "Mariam",
+                age: 32
+            },
+            {
+                _id:10,
+                name: 'rami',
+                age: 24
+            },
+            {
+                _id: 11,
+                name: 'sara',
+                age:30
+            },
+            {
+                _id: 12,
+                name: "adel",
+                age: 40
+            }
+        ], (error, data) =>
+    {
+        if (error)
+        {
+            console.log("Error in inserting multiple documents");
+        }
+        console.log(data.insertedCount)
+    }
+    )
     /////////////////////////
     //find match age =27
     db.collection('users').find({ age: 27 }).toArray((error, users) =>
@@ -166,13 +166,6 @@ MongoClient.connect(url, (error, res1) =>
     // })
     //     .then((data1) => { console.log(data1.modifiedCount) })
     //     .catch((error) => { console.log(error) });
-    /////////////////////////
-    //$inc=4 for first 4doc age =27
-    db.collection('users').insertMany({ age: 27 }, { $inc: { age: 4 } }).limit(4)
-        .then((data1) => { console.log(data1.modifiedCount) })
-        .catch((error) => { console.log(error) });
-
-
     //////////////////////////////////////////////
     // updatemany $inc =10 for age=27
     db.collection('users').updateMany({ age: 31 },
